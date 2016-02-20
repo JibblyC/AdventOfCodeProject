127a13088cf20c6f82895fb7cb3e2233aea4f2d0
Ciaran Doyle <irishrocker182@gmail.com>
2016-02-20 13:31:33 +0000

    Day 2 Challenge 2 completed

--- src/com/testProject/adventofcode/day2Challenge.java
+++ src/com/testProject/adventofcode/day2Challenge.java
@@ -8,45 +8,68 @@ import java.util.regex.Pattern;
 /**
  * Created by Ciaran on 19/02/2016.
  *
- * Challange Overview :
- *
- * --- Day 2: I Was Told There Would Be No Math ---
-
- The elves are running low on wrapping paper, and so they need to submit an order for more. They have a list of the dimensions (length l, width w, and height h) of each present, and only want to order exactly as much as they need.
-
- Fortunately, every present is a box (a perfect right rectangular prism), which makes calculating the required wrapping paper for each gift a little easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. The elves also need a little extra paper for each present: the area of the smallest side.
-
- For example:
-
- A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
- A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
- All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
  */
 public class Day2Challenge {
+
+    private int length = 0;
+    private int width = 0;
+    private int height = 0;
+    int totalWrappingPaperNeeded = 0;
+    int totalRibbonNeeded = 0;
+
     public static void main(String[] args) throws IOException {
 
-
         FileIOUtil fileIOUtil = new FileIOUtil();
-
         String[] arrayOfNumbers = fileIOUtil.readFileIntoArray("C:\\Users\\Ciaran\\Documents\\ClutterStuff\\GenericTextFiles\\InputforDay2Challenge.txt");
-        int totalSurfaceArea = 0;
 
-        for (int i = 0; i < arrayOfNumbers.length ; i++) {
+        Day2Challenge day2Challenge = new Day2Challenge();
+        day2Challenge.calculateTotalPaperAndRibbon(arrayOfNumbers);
+        System.out.println("Total Wrapping Paper Needed : " + day2Challenge.getTotalWrappingPaperNeeded());
+        System.out.println("Total Ribbon Paper Needed : " + day2Challenge.getTotalRibbonNeeded());
+    }
+    public void calculateTotalPaperAndRibbon(String[] inputFromFile) {
 
-            String[] dimensions = arrayOfNumbers[i].split(Pattern.quote("x"));
+        for (int i = 0; i < inputFromFile.length; i++) {
 
-            int length = Integer.parseInt(dimensions[0]);
-            int width = Integer.parseInt(dimensions[1]);
-            int height = Integer.parseInt(dimensions[2]);
-            int sOne = (length * width);
-            int sTwo = (width * height);
-            int sThree = (length * height);
-
-            int smallestArea = Math.min(Math.min(sOne,sTwo),sThree);
-
-            totalSurfaceArea +=  smallestArea + (2*sOne) + (2*sTwo) + (2*sThree);
+            populateDimensions(inputFromFile[i]);
+            calculateTotalWrappingPaper();
+            calculateTotalRibbonNeeded();
         }
 
-        System.out.print("Total Surface Area : " + totalSurfaceArea);
+    }
+
+    private void calculateTotalWrappingPaper(){
+        int sOne = (length * width);
+        int sTwo = (width * height);
+        int sThree = (length * height);
+
+        int smallestArea = Math.min(Math.min(sOne, sTwo), sThree);
+        totalWrappingPaperNeeded += smallestArea + (2 * sOne) + (2 * sTwo) + (2 * sThree);
+    }
+
+    private void calculateTotalRibbonNeeded(){
+
+        int smallestSides = Math.min(Math.min((length + width),(height + width)),(height + length));
+
+
+        int ribbonToWrap = (2 * smallestSides) + (length * width * height);
+        totalRibbonNeeded += ribbonToWrap;
+        System.out.println(ribbonToWrap);
+    }
+
+    private void populateDimensions(String inputLine) {
+
+        String[] dimensions = inputLine.split(Pattern.quote("x"));
+        length = Integer.parseInt(dimensions[0]);
+        width = Integer.parseInt(dimensions[1]);
+        height = Integer.parseInt(dimensions[2]);
+
+    }
+    public int getTotalWrappingPaperNeeded() {
+        return totalWrappingPaperNeeded;
+    }
+
+    public int getTotalRibbonNeeded() {
+        return totalRibbonNeeded;
     }
 }
